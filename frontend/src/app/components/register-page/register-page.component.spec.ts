d8c95107da620243ebf81eb434bb7365ab823138
Vincent Perry Divino <156486099+vpmdivino@users.noreply.github.com>
2025-11-11 00:50:43 +0800

    Add files via upload

---
+++ frontend/src/app/components/register-page/register-page.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+
+import { RegisterPageComponent } from './register-page.component';
+
+describe('RegisterPageComponent', () => {
+  let component: RegisterPageComponent;
+  let fixture: ComponentFixture<RegisterPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RegisterPageComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule, FormsModule]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegisterPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
